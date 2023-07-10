758503559928a68e51152966c2fcd4d869dfaf08
risarora <risarora@gmail.com>
2023-07-10 21:01:20 +0530

    UI Fix
    Made progress Percentage non clickable

--- src/App.js
+++ src/App.js
@@ -2,10 +2,26 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 
 const newimages = [
-  { src: require("./images/desktop-high-res.jpg"), progress: 90 },
-  { src: require("./images/desktop-high-res.jpg"), progress: 0 },
-  { src: require("./images/enterprise-high-res.jpg"), progress: 50 },
-  { src: require("./images/mobile-high-res.jpg"), progress: 50 },
+  {
+    src: require("./images/desktop-high-res.jpg"),
+    progress: 90,
+    link: "https://example.com/image1",
+  },
+  {
+    src: require("./images/desktop-high-res.jpg"),
+    progress: 0,
+    link: "https://example.com/image1",
+  },
+  {
+    src: require("./images/enterprise-high-res.jpg"),
+    progress: 50,
+    link: "https://example.com/image1",
+  },
+  {
+    src: require("./images/mobile-high-res.jpg"),
+    progress: 50,
+    link: "https://example.com/image1",
+  },
 ];
 
 const Grid = () => {
@@ -13,17 +29,24 @@ const Grid = () => {
     <div className="image-grid">
       {newimages.map((image, index) => (
         <div className="image-container" key={index}>
-          <img
-            src={image.src}
-            alt={`Image ${index + 1}`}
-            className={`image ${image.progress === 0 ? "gray" : ""}`}
-          />
-          <div className="progress-bar">
-            <div
-              className="progress-bar-inner"
-              style={{ width: `${image.progress}%` }}
-            ></div>
-          </div>
+          <a
+            href={image.link}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="image-link"
+          >
+            <img
+              src={image.src}
+              alt={`Image ${index + 1}`}
+              className={`image ${image.progress === 0 ? "gray" : ""}`}
+            />
+            <div className="progress-bar">
+              <div
+                className="progress-bar-inner"
+                style={{ width: `${image.progress}%` }}
+              ></div>
+            </div>
+          </a>
           <div className="percentage">{`${image.progress}%`}</div>
         </div>
       ))}
